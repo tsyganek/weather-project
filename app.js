39f025816487b99583e892e012b788e2073c676d
Kateryna Tsygankova <kateryna.tsygankova.se@gmail.com>
2022-07-23 16:05:43 +0200

    Weather search engine added

---
+++ app.js
@@ -0,0 +1,29 @@
+// weather search engine
+
+const apiKey = "0f128b06bf582bcfa96722c9eae3c85c";
+let apiUrlbyCityName = `https://api.openweathermap.org/data/2.5/weather?q=Kyiv&appid=${apiKey}&units=metric`;
+
+let apiUrlbyCoordinates = `https://api.openweathermap.org/data/2.5/weather?lat={lat}&lon={lon}&appid=${apiKey}&units=metric`;
+
+function displayWeather(response) {
+  console.log(response);
+  console.log(response.data.weather[0].description);
+  let temperatureElement = document.querySelector("#temperature");
+  temperatureElement.innerHTML = `${Math.round(response.data.main.temp)}°`;
+
+  let humidityElement = document.querySelector("#humidity");
+  humidityElement.innerHTML = `Humidity: ${response.data.main.humidity}%`;
+
+  let windElement = document.querySelector("#wind");
+  windElement.innerHTML = `Wind: ${response.data.wind.speed}km/h`;
+
+  let feelsLikeElement = document.querySelector("#feels-like");
+  feelsLikeElement.innerHTML = `Feels like: ${Math.round(
+    response.data.main.feels_like
+  )}°`;
+
+  let descriptionElement = document.querySelector("#description");
+  descriptionElement.innerHTML = response.data.weather[0].description;
+}
+
+axios.get(apiUrlbyCityName).then(displayWeather);
