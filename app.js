ade7195e892f633282fc21f3b52ee857475de377
Kateryna Tsygankova <kateryna.tsygankova.se@gmail.com>
2022-07-24 12:52:24 +0200

    Current weather icon fixed

--- app.js
+++ app.js
@@ -1,5 +1,7 @@
 //actual time
 
+let city = "new york";
+
 function formatDate(timestamp) {
   let date = new Date(timestamp);
   let hours = date.getHours();
@@ -26,13 +28,23 @@ function formatDate(timestamp) {
 // weather search engine
 
 const apiKey = "0f128b06bf582bcfa96722c9eae3c85c";
-let apiUrlbyCityName = `https://api.openweathermap.org/data/2.5/weather?q=Kyiv&appid=${apiKey}&units=metric`;
+let apiUrlbyCityName = `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${apiKey}&units=metric`;
 
 let apiUrlbyCoordinates = `https://api.openweathermap.org/data/2.5/weather?lat={lat}&lon={lon}&appid=${apiKey}&units=metric`;
 
 function displayWeather(response) {
   console.log(response);
-  console.log(response.data.weather[0].description);
+
+  let weatherIconElement = document.querySelector("#icon");
+  weatherIconElement.setAttribute(
+    "src",
+    `http://openweathermap.org/img/wn/${response.data.weather[0].icon}.png`
+  );
+  weatherIconElement.setAttribute(
+    "alt",
+    `${response.data.weather[0].icon.description}`
+  );
+
   let temperatureElement = document.querySelector("#temperature");
   temperatureElement.innerHTML = `${Math.round(response.data.main.temp)}°`;
 
